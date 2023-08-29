74f11f5b58eb51382df1906cc5c176af0055948d
EmilyG345 <101951506+EmilyG345@users.noreply.github.com>
2023-08-29 21:33:48 +0100

    Update Rad to Deg.py

--- Rad to Deg.py
+++ Rad to Deg.py
@@ -1,38 +1,3 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Jul 30 16:02:48 2023
-
-@author: emilygoff
-"""
-
-import math
-print(math.pi)
-
-#user input rad value
-rad = int(input("Please enter radian value"))
-
-#print("Your number is ",rad)
-
-deg = rad * (180/math.pi)
-print(deg)
-
-
-
-###Create a function. Works but doesn't handle errors such as handling letters
-
-def getrad():
-    rad = int(input("Please enter radian value"))
-    if rad:
-        deg = rad * (180/math.pi)
-        print(deg)
-    else:
-        print("No number provided")
-    return
-        
-getrad()
-    
-#second attempt
 def getrad1():
     while True:
         try:
